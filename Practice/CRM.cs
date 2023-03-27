c9fd26aad305d87b8b60eb533ad90d805af2437e
Yrmen456 <120619041+Yrmen456@users.noreply.github.com>
2023-03-27 14:23:49 +0500

    Update CRM.cs

--- Practice/CRM.cs
+++ Practice/CRM.cs
@@ -14,7 +14,7 @@ namespace Practice
     {
         public CRM()
         {
-            InitializeComponent();
+            InitializeComponent();//
         }
 
         private void button3_Click(object sender, EventArgs e)
